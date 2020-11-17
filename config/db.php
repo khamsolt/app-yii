b1d637166429fe2289dd4ba1aa80ab328397be19
khamsolt <dustum-khan@inbox.ru>
2020-11-17 16:04:21 +0300

    #master Setting yii framework.

--- config/db.php
+++ config/db.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=postgres;port=5432;dbname=app_yii_db',
-    'username' => 'khamsolt',
-    'password' => 'khamsolt',
-    'charset' => 'utf8',
+  'class' => 'yii\db\Connection',
+  'dsn' => 'pgsql:host=postgres;port=5432;dbname=app_yii_db',
+  'username' => 'khamsolt',
+  'password' => 'khamsolt',
+  'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+  // Schema cache options (for production environment)
+  //'enableSchemaCache' => true,
+  //'schemaCacheDuration' => 60,
+  //'schemaCache' => 'cache',
 ];
--- config/params.php
+++ config/params.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+  'adminEmail' => 'admin@example.com',
+  'senderEmail' => 'noreply@example.com',
+  'senderName' => 'Example.com mailer',
+  'seeders' => [
+    \app\seeders\ProductSeeder::class,
+  ],
 ];
